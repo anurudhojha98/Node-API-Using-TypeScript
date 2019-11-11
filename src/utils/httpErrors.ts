a355e0fe25a6ba3bf3fe6f8e584ba73afca49982
anurudhojha98 <anurudhojha98@gmail.com>
2019-11-11 10:06:03 +0530

    Project setup

---
+++ src/utils/httpErrors.ts
@@ -0,0 +1,29 @@
+export abstract class HTTPClientError extends Error {
+    readonly statusCode!: number;
+    readonly name!: string;
+  
+    constructor(message: object | string) {
+      if (message instanceof Object) {
+        super(JSON.stringify(message));
+      } else {
+        super(message);
+      }
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  export class HTTP404Error extends Error {
+    readonly statusCode!: number;
+    readonly name!: string;
+  
+    constructor(message: object | string) {
+      if (message instanceof Object) {
+        super(JSON.stringify(message));
+      } else {
+        super(message);
+      }
+      this.name = this.constructor.name;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
